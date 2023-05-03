55c0522b860f6ab46330143854313cc6456b78da
slakerdriper <129144117+slakerdriper@users.noreply.github.com>
2023-05-03 10:17:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -21,26 +21,26 @@ global.lApiKey = [ // if this API key get expired then please go to https://api.
 ]
 
 
-global.botname = "Queen Anya" 
-global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
+global.botname = "Queen Sheqow" 
+global.author = "slate"  
+global.packname = " slate" 
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
 global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.footer = "© Queen Anya Bot"
-global.prefa = ['-'] // single prefix
+global.footer = "© Queen Sheqow Bot"
+global.prefa = ['.'] // single prefix
 global.themeemoji = '🔥'
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['918811074852'] 
+global.ownername = "slate"   // //        
+global.ownernumber = ['254755894032'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
-global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
+global.continent = "Africa" // your continent name 
+global.region = "Kenya" // your country name
+global.state = "Nairobi" // your state name
+global.district = "Kitisuru" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "3.69_pika" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
@@ -54,7 +54,7 @@ global.greet4 = "Good night 😴" // after 8:30 PM
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
+global.premium = ['916900******']  // available soon
 global.sudo = ['916900******','91881107****']  // Co - Owners
 
 
@@ -75,7 +75,7 @@ global.autoTyping = false
 global.autoreadpmngc = false 
 global.autoReadGc = false 
 global.autoRecord = false 
-global.available = true
+global.available = false
 global.autoTyping = false 
 
 //----------- Users Userlimit -----------\\
